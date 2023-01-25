2605c92f1f10e31d65777a1f8925d845cfeee6b4
Mrowr Purr <mrowr.purr@gmail.com>
2023-01-25 06:11:41 -0800

    Cleanup unnecessary comments

--- plugin.cpp
+++ plugin.cpp
@@ -1,16 +1,12 @@
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
 
-    // SKSE PLUGIN INITIALIZATION CODE BELOW
-    //
     // This example prints "Hello, world!" to the Skyrim ~ console.
     // To view it, open the ~ console from the Skyrim Main Menu.
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
         if (message->type == SKSE::MessagingInterface::kDataLoaded)
             RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
     });
-    //
-    // SKSE PLUGIN INITIALIZATION CODE ABOVE
 
     return true;
 }
